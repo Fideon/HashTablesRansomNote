1ec78136670b61459feb70dd9f9108bac396cfeb
Fidel Salazar <fidelsf91@gmail.com>
2020-11-15 17:42:03 -0600

    first solution

--- HashTablesRansomNote/Program.cs
+++ HashTablesRansomNote/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashTablesRansomNote
 {
     class Program
     {
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            HashSet<string> magDict = new HashSet<string>(magazine);
+
+            bool match = false;
+
+            foreach(string s in note)
+            {
+                match = magDict.Remove(s);
+                if (match == false) break;
+            }
+
+            Console.WriteLine(match ? "Yes" : "No");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            string[] magazine = Console.ReadLine().Split(' ');
+
+            string[] note = Console.ReadLine().Split(' ');
+
+            checkMagazine(magazine, note);
         }
     }
 }
