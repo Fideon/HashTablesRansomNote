13f6da3a1e1ae898a5b2395d3dfc4313d68466da
Fidel Salazar <fidelsf91@gmail.com>
2020-11-15 17:18:57 -0600

    Project initialization

---
+++ HashTablesRansomNote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTablesRansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
